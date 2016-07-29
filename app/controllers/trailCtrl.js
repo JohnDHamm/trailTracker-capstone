ea04488ad40eedac10bf684d3e288369bec31ffc
John Hamm <johndhamm1@gmail.com>
2016-07-29 09:29:40 -0500

    some styling tests

--- app/controllers/trailCtrl.js
+++ app/controllers/trailCtrl.js
@@ -3,11 +3,12 @@
 app.controller("trailCtrl", function($scope, DatabaseFactory){
 	
 	console.log("switched to trail page");
+	$scope.trailColor = "light-green darken-2"
 	// let trailList = [];
 
 	// DatabaseFactory.getTrailList()
 	// 	.then(function(trails){
 	// 		$scope.trailList =  trails;
 	// 	});
-		
+
 });
