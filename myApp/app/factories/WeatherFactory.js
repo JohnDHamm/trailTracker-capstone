ebb911cc357bcd7558a1f367a596395718d83431
John Hamm <johndhamm1@gmail.com>
2016-10-06 23:17:33 -0500

    deployed on firebase

--- myApp/app/factories/WeatherFactory.js
+++ myApp/app/factories/WeatherFactory.js
@@ -5,15 +5,14 @@ app.factory("WeatherFactory", function($q, $http) {
 	function getCurrentWeather(lat, long){
 		return new Promise(function(resolve, reject){
 			$.ajax({
-				url: `http://api.wunderground.com/api/f86d6fa04b4dd2cd/conditions/q/${lat},${long}.json`
+				// url: `http://api.wunderground.com/api/f86d6fa04b4dd2cd/conditions/q/${lat},${long}.json`
+				url: `https://trailweatherproxy.herokuapp.com/${lat},${long}`
 			}).done(function(data){
-				resolve(data);
+				resolve(JSON.parse(data));
 			});
 		});
 	}
 
-
-
 	return {getCurrentWeather};
-	
+
 });
