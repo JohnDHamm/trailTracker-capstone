fd0132f080e10cd12d8b6541952c40cddfac9f84
John Hamm <johndhamm1@gmail.com>
2016-12-12 15:03:04 -0600

    open ticket badges added, hidden if no open tickets

--- myApp/app/controllers/selectTrailCtrl.js
+++ myApp/app/controllers/selectTrailCtrl.js
@@ -1,12 +1,24 @@
 "use strict";
 
 app.controller("selectTrailCtrl", function($scope, DatabaseFactory){
-	
+
 	let trailList = [];
 
 	DatabaseFactory.getTrailList()
 		.then(function(trails){
-			$scope.trailList =  trails;
+			console.log("trails", trails);
+			console.log("HC", trails[0].trailName);
+			trails.forEach(function () {
+				console.log("trailName", trails.trailName);
+			})
+			for (let i = 0; i < trails.length; i++) {
+				console.log("trails", i, trails[i].numOpenTickets);
+				if (trails[i].numOpenTickets !== 0) {
+					trails[i].showBadge = true;
+					console.log("showBadge", i, trails[i].showBadge);
+				}
+ 			}
+			$scope.trailList = trails;
 		});
 
 
